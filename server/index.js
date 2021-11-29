e6f76be01272b74de476a12540931d19dae7bb36
anjvyas <anjvyas@umich.edu>
2021-11-28 18:44:06 -0800

    pushing node server changes that allow pulling ticket list from api

--- server/index.js
+++ server/index.js
@@ -1,14 +1,31 @@
-// need to require this package so we can read environment variables
-require('dotenv').config()
+// importing required packages
 
+// dotenv will help us read environment variables
+require('dotenv').config()
 const express = require("express");
+const axios = require('axios');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-app.get("https://zccanjalivyas.zendesk.com/api/v2/tickets", (req, res) => {
-    res.json({ message: "Hello from server!" });
+// constructing the authorization header based on the username and zendesk api token
+const auth_header = "Basic " + Buffer.from(process.env.USERNAME + "/" + "token:" + process.env.API_KEY).toString('base64')
+// constructing the url to fetch data from based on the specified subdomain
+const url = "https://" + process.env.SUBDOMAIN + ".zendesk.com/api/v2/tickets"
+
+app.get("/home", (req, res) => {
+  axios.get(url, {
+    headers: {
+      'Authorization': auth_header
+    }
+  })
+    .then(res1 => {
+      res.send(res1.data.tickets);
+    })
+    .catch(err =>
+      {
+      res.send(err);
+    })
 });
 
 app.listen(PORT, () => {
